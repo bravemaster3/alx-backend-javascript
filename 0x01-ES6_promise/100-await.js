264f524e895e818a19ce58b7b224205f581280bb
Koffi Dodji Noumonvi <39010414+bravemaster3@users.noreply.github.com>
2024-04-09 19:34:42 +0000

    Added a try catch

--- 0x01-ES6_promise/100-await.js
+++ 0x01-ES6_promise/100-await.js
@@ -1,8 +1,14 @@
 import { uploadPhoto, createUser } from './utils';
 
 export default async function asyncUploadUser() {
-  return ({
-    photo: await uploadPhoto(),
-    user: await createUser(),
-  });
+  let photo;
+  let user;
+  try {
+    photo = await uploadPhoto();
+    user = await createUser();
+  } catch (error) {
+    photo = null;
+    user = null;
+  }
+  return ({ photo, user });
 }
