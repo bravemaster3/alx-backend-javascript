9f3f17a7dbb452fb7a0e2f1c0c8a30d9ce6ffe79
Koffi Dodji Noumonvi <39010414+bravemaster3@users.noreply.github.com>
2024-04-09 19:10:06 +0000

    Add 8

--- 0x01-ES6_promise/8-try.js
+++ 0x01-ES6_promise/8-try.js
@@ -1,10 +1,6 @@
 export default function divideFunction(numerator, denominator) {
-  let result;
-  try {
-    if (denominator === 0) {
-      throw new Error('cannot divide by 0');
-    }
-    result = numerator / denominator;
-  } catch (error) { console.error(error); }
-  return result;
+  if (denominator === 0) {
+    throw new Error('cannot divide by 0');
+  }
+  return numerator / denominator;
 }
