13a6fab244701e0d6be4b0c84408f4753f076dac
Koffi Dodji Noumonvi <39010414+bravemaster3@users.noreply.github.com>
2024-04-09 18:41:53 +0000

    Add 6

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -4,11 +4,13 @@ import uploadPhoto from './5-photo-reject';
 export default function handleProfileSignup(firstName, lastName, fileName) {
   return Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
     .then((results) => {
-      results.forEach((item) => {
+      results.forEach((itemOri) => {
+        const item = itemOri;
         if (item.status === 'rejected') {
-          item['value'] = item['reason'];
-          delete item['reason'];
+          item.value = item.reason;
+          delete item.reason;
         }
+        return (item);
       });
       console.log(results);
     });
