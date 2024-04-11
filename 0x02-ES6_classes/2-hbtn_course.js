1ef095ce4bf397c6b3b0aa83d0cdb4ee05ee2aa0
Koffi Dodji Noumonvi <39010414+bravemaster3@users.noreply.github.com>
2024-04-11 14:13:45 +0000

    Add 2

---
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -0,0 +1,49 @@
+export default class HolbertonCourse {
+  constructor(name, length, students) {
+    if (typeof name === 'string') {
+      this._name = name;
+    }
+    if (typeof length === 'number') {
+      this._length = length;
+    }
+    if (Array.isArray(students) && students.every((st) => typeof st === 'string')) {
+      this._students = students;
+    }
+  }
+
+  get name() {
+    return (this._name);
+  }
+
+  set name(newName) {
+    if (typeof newName === 'string') {
+      this._name = newName;
+    } else {
+      throw TypeError('Name must be a string');
+    }
+  }
+
+  get length() {
+    return (this._length);
+  }
+
+  set length(newLength) {
+    if (typeof newLength === 'number') {
+      this._length = newLength;
+    } else {
+      throw TypeError('Length must be a number');
+    }
+  }
+
+  get students() {
+    return (this._students);
+  }
+
+  set students(newStudents) {
+    if (Array.isArray(newStudents) && newStudents.every((st) => typeof st === 'string')) {
+      this._students = newStudents;
+    } else {
+      throw TypeError('Students must be an array of strings');
+    }
+  }
+}
