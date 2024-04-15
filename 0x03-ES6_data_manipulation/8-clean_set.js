584ae681acb93fa7e43d2ca18e923fef1bf735e0
Koffi Dodji Noumonvi <39010414+bravemaster3@users.noreply.github.com>
2024-04-15 17:37:17 +0000

    Add 8

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -3,8 +3,10 @@ export default function cleanSet(set, startString) {
   if (startString.length === 0) {
     return '';
   }
-  const finalString = myArray.filter((x) => x.startsWith(startString))
+  const finalString = myArray
+    .filter((x) => x.startsWith(startString))
     .map((x) => x.slice(startString.length))
+    .filter((x) => x.length > 0)
     .join('-');
   return (finalString);
 }
