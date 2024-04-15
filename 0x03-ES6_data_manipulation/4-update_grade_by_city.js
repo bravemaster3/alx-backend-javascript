5cf837d14be2d38cdb5359cec8e880b49ba6d517
Koffi Dodji Noumonvi <39010414+bravemaster3@users.noreply.github.com>
2024-04-15 05:59:09 +0000

    Edited 4

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -2,9 +2,8 @@ export default function updateStudentGradeByCity(listOfStudents, city, newGrades
   const newList = listOfStudents.filter((st) => st.location === city)
     .map((st) => {
       const matchingGrade = newGrades.find((gr) => gr.studentId === st.id);
-      const updatedSt = st;
-      updatedSt.grade = matchingGrade !== undefined ? matchingGrade.grade : 'N/A';
-      return (updatedSt);
+      const stGrade = matchingGrade ? matchingGrade.grade : 'N/A';
+      return { ...st, grade: stGrade };
     });
   return newList;
 }
