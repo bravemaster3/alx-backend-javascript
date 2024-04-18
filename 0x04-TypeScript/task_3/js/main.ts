34b95d3e347f6ceb8ebbe063ec94bb297d064b67
Koffi Dodji Noumonvi <39010414+bravemaster3@users.noreply.github.com>
2024-04-18 02:13:34 +0000

    Add 8

---
+++ 0x04-TypeScript/task_3/js/main.ts
@@ -0,0 +1,13 @@
+/// <reference path='./crud.d.ts'/>
+import * as CRUD from './crud';
+import { RowElement, RowID } from './interface.js';
+
+let row: RowElement = {
+  firstName: 'Guillaume',
+  lastName: 'Salva',
+}
+const newRowID: RowID = CRUD.insertRow(row);
+const updateRow: RowElement = { ...row, age: 23 };
+CRUD.updateRow(newRowID, updateRow);
+
+CRUD.deleteRow(newRowID);
