557e5bb006cc29e2d567c0feb241b7bdbb7c2ad4
Koffi Dodji Noumonvi <39010414+bravemaster3@users.noreply.github.com>
2024-04-18 03:20:50 +0000

    Add 5

--- 0x04-TypeScript/task_4/js/main.ts
+++ 0x04-TypeScript/task_4/js/main.ts
@@ -1,98 +1,29 @@
+import { Subjects } from './subjects/Subject';
 
-interface CommonInterface {
-  workFromHome(): string;
-  getCoffeeBreak(): string;
+export const cpp = new Subjects.Cpp();
+export const java = new Subjects.Java();
+export const react = new Subjects.React();
+
+export const cTeacher: Subjects.Teacher = {
+  firstName: 'John',
+  lastName: 'Doe',
+  experienceTeachingC: 10,
 }
 
-interface DirectorInterface extends CommonInterface {
-  workDirectorTasks(): string;
-}
+// For Cpp subject
+console.log("C++");
+cpp.setTeacher(cTeacher);
+console.log(cpp.getRequirements());
+console.log(cpp.getAvailableTeacher());
 
-interface TeacherInterface extends CommonInterface {
-  workTeacherTasks(): string;
-}
+// For Java subject
+console.log("Java");
+java.setTeacher(cTeacher);
+console.log(java.getRequirements());
+console.log(java.getAvailableTeacher());
 
-class Director implements DirectorInterface {
-  constructor() {
-  }
-
-  workFromHome(): string {
-    return 'Working from home';
-  }
-
-  getCoffeeBreak(): string {
-    return 'Getting a coffee break';
-  }
-
-  workDirectorTasks(): string {
-    return 'Getting to director tasks';
-  }
-
-  toString(): string {
-    return 'Director';
-  }
-}
-
-
-class Teacher implements TeacherInterface {
-  constructor() {
-  }
-
-  workFromHome(): string {
-    return 'Cannot work from home';
-  }
-
-  getCoffeeBreak(): string {
-    return 'Cannot have a break';
-  }
-
-  workTeacherTasks(): string {
-    return 'Getting to work';
-  }
-
-  toString(): string {
-    return 'Teacher';
-  }
-}
-
-function createEmployee(salary: number | string) {
-  if (typeof salary === 'number' && salary < 500) {
-    const teacher1 = new Teacher();
-    return teacher1;
-  } else {
-    const director1 = new Director();
-    return director1;
-  }
-}
-
-function isDirector(employee: Teacher | Director): boolean {
-  return employee instanceof Director;
-}
-
-function executeWork(employee: Teacher | Director) {
-  if (isDirector(employee)) {
-    console.log((employee as Director).workDirectorTasks());
-  } else {
-    console.log((employee as Teacher).workTeacherTasks());
-  }
-}
-
-type Subjects = 'Math' | 'History';
-
-function teachClass(todayClass: Subjects) {
-  const allowedClasses: Subjects[] = ['Math', 'History'];
-
-  if (allowedClasses.includes(todayClass)) {
-    return `Teaching ${todayClass}`;
-  }
-}
-///TESTS
-console.log(createEmployee(200));
-console.log(createEmployee(1000));
-console.log(createEmployee('$500'));
-
-executeWork(createEmployee(200));
-executeWork(createEmployee(1000));
-
-console.log(teachClass('Math'));
-console.log(teachClass('History'));
+// For React subject
+console.log("React");
+react.setTeacher(cTeacher);
+console.log(react.getRequirements());
+console.log(react.getAvailableTeacher());
