b87ea76e1a85bd3905a4d5de1159e26fc735902b
Koffi Dodji Noumonvi <39010414+bravemaster3@users.noreply.github.com>
2024-06-04 04:37:06 +0000

    corrected 1

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,7 +1,7 @@
-console.log('Welcome to Holberton School, what is your name?');
-process.stdin.on('data', (data) => {
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
+process.stdin.on('readable', () => {
   const name = process.stdin.read();
-  console.log(`Your name is: ${name}`);
+  process.stdout.write(`Your name is: ${name}`);
   if (!process.stdin.isTTY) {
     console.log('This important software is now closing');
   }
