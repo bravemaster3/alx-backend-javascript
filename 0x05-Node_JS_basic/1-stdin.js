59808dc67ae9b254e95e4f93b9878dbfc7cca549
Koffi Dodji Noumonvi <39010414+bravemaster3@users.noreply.github.com>
2024-06-04 04:32:59 +0000

    corrected 1

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,6 +1,6 @@
 console.log('Welcome to Holberton School, what is your name?');
 process.stdin.on('data', (data) => {
-  const name = data.toString().trim();
+  const name = process.stdin.read();
   console.log(`Your name is: ${name}`);
   if (!process.stdin.isTTY) {
     console.log('This important software is now closing');
