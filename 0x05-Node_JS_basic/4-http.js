35d0892350a64bef306c313ddee3195417aaef74
Koffi Dodji Noumonvi <bravemaster102@gmail.com>
2024-06-05 01:15:37 +0200

    httm server

---
+++ 0x05-Node_JS_basic/4-http.js
@@ -0,0 +1,16 @@
+const http = require('http');
+
+// Create the HTTP server
+const app = http.createServer((req, res) => {
+  // Set the response header to plain text
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  // Send the response body
+  res.end('Hello Holberton School!');
+});
+
+// Listen on port 1245
+app.listen(1245, () => {
+  console.log('Server is listening on port 1245');
+});
+
+module.exports = app;
