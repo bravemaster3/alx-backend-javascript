23d954b1f6f1daaf94aee876e86dc8f35f2b7036
Koffi Dodji Noumonvi <bravemaster102@gmail.com>
2024-06-05 02:27:38 +0200

    add 6

--- 0x05-Node_JS_basic/7-http_express.js
+++ 0x05-Node_JS_basic/7-http_express.js
@@ -1,6 +1,43 @@
 // Importing required modules
 const express = require('express');
-const countStudents = require('./3-read_file_async');
+const fs = require('fs').promises;
+
+function countStudents(path) {
+  return fs.readFile(path, 'utf8')
+    .then((data) => {
+      const lines = data.split('\n').filter((line) => line.trim() !== '');
+      if (lines.length <= 1) {
+        return 'Number of students: 0';
+      }
+
+      const students = lines.slice(1).map((line) => line.split(','));
+      const totalStudents = `Number of students: ${students.length}`;
+      const fields = {};
+
+      students.forEach((student) => {
+        const field = student[3];
+        const firstName = student[0];
+        if (!fields[field]) {
+          fields[field] = [];
+        }
+        fields[field].push(firstName);
+      });
+
+      let result = `${totalStudents}\n`;
+      for (const field in fields) {
+        if (field) {
+          const count = fields[field].length;
+          const names = fields[field].join(', ');
+          result += `Number of students in ${field}: ${count}. List: ${names}\n`;
+        }
+      }
+
+      return result.trim();
+    })
+    .catch(() => {
+      throw new Error('Cannot load the database');
+    });
+}
 
 // Creating an instance of Express
 const app = express();
@@ -13,19 +50,19 @@ app.get('/', (req, res) => {
 
 // Define a route for the endpoint "/students"
 app.get('/students', (req, res) => {
-  let database = req.query.database || 'database.csv';
+  const database = process.argv[2] || 'database.csv';
 
   countStudents(database)
-    .then(result => {
+    .then((result) => {
       res.send(`This is the list of our students\n${result}`);
     })
-    .catch(error => {
+    .catch((error) => {
       res.send(`This is the list of our students\n${error.message}`);
     });
 });
 
 // Start the server and listen on port 1245
-const server = app.listen(1245, () => {
+app.listen(1245, () => {
   console.log('Server is listening on port 1245');
 });
 
