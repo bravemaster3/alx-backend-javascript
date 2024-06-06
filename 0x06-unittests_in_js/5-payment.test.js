8a5de58e31777f203bfb564500af4f7beb0f1352
Koffi Dodji Noumonvi <bravemaster102@gmail.com>
2024-06-06 09:42:39 +0200

    Add 5

---
+++ 0x06-unittests_in_js/5-payment.test.js
@@ -0,0 +1,26 @@
+const sinon = require('sinon');
+const chai = require('chai');
+const expect = chai.expect;
+const sendPaymentRequestToApi = require('./5-payment');
+
+describe('sendPaymentRequestToApi', function() {
+  let consoleLogSpy;
+
+  beforeEach(function() {
+    consoleLogSpy = sinon.spy(console, 'log');
+  });
+
+  afterEach(function() {
+    consoleLogSpy.restore();
+  });
+
+  it('should log "The total is: 120" when called with 100 and 20', function() {
+    sendPaymentRequestToApi(100, 20);
+    expect(consoleLogSpy.calledOnceWithExactly('The total is: 120')).to.be.true;
+  });
+
+  it('should log "The total is: 20" when called with 10 and 10', function() {
+    sendPaymentRequestToApi(10, 10);
+    expect(consoleLogSpy.calledOnceWithExactly('The total is: 20')).to.be.true;
+  });
+});
