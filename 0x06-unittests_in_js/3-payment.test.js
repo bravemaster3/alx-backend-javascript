41bed046d4d4828d62db2eef1fb0fecf902b8799
Koffi Dodji Noumonvi <bravemaster102@gmail.com>
2024-06-06 09:16:51 +0200

    Add 3

---
+++ 0x06-unittests_in_js/3-payment.test.js
@@ -0,0 +1,22 @@
+const sinon = require('sinon');
+const chai = require('chai');
+const expect = chai.expect;
+const Utils = require('./utils');
+const sendPaymentRequestToApi = require('./3-payment');
+
+describe('sendPaymentRequestToApi', function() {
+  let spy;
+
+  beforeEach(function() {
+    spy = sinon.spy(Utils, 'calculateNumber');
+  });
+
+  afterEach(function() {
+    spy.restore();
+  });
+
+  it('should call Utils.calculateNumber with SUM, 100, and 20', function() {
+    sendPaymentRequestToApi(100, 20);
+    expect(spy.calledOnceWithExactly('SUM', 100, 20)).to.be.true;
+  });
+});
